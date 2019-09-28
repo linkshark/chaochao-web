6492526d375f96c899a031513ae9b9142c06946b
shark <sharkshen@outlook.com>
2019-09-28 16:16:58 +0800

    Initial commit

---
+++ src/components/Index.vue
@@ -0,0 +1,19 @@
+<template><meta charset="UTF-8">
+  <title>欢迎使用webTalk聊天应用</title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+  <link rel="icon" sizes="any" href="/static/images/logo.png">
+  <link rel="stylesheet" href="//at.alicdn.com/t/font_737314_ykp1exp92.css">
+  <link rel="stylesheet" href="/static/css/ui-player.css">
+  <link rel="stylesheet" href="/static/css/webChat.css"></template>
+<script>
+    export default {
+        name: "Index"
+    }
+</script>
+
+<style scoped>
+
+</style>
